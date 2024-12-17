adeeeae82c2e1cb26d51e63e1e9984a934c91645
Alessandro Montanari <alessandro.montanari@soluzionifutura.it>
2024-12-17 15:19:46 +0100

    add test file

---
+++ test/index.ts
@@ -0,0 +1,30 @@
+import Sqlite from "better-sqlite3"
+import OpenAI from "openai"
+import configs from "../src/configs"
+
+const db = new Sqlite("data.db")
+const openai = new OpenAI({
+    apiKey: configs.OPENAI_API_KEY
+})
+
+const main = async () => {
+    /* 
+        Put here the code you want to run without the telegram wrapper.
+
+        You can run this file with the command `npm run test`
+
+        If you want to run another file, you can run the command: `node --import tsx --import dotenv/config <your-file-path>`
+    */
+
+    const res = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [
+            { role: "system", content: "You are a helpful assistant." },
+            { role: "user", content: "What is the meaning of life?" }
+        ]
+    })
+
+    console.dir(res, { depth: null })
+}
+
+main()
